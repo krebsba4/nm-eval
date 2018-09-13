8806344f370ca834843a9ef2f8564b6126c4f685
Ryan <Ryan@Ryan-PC.lan1>
2018-09-12 20:26:08 -0500

    test rest endpoint now returns a list of 50 random strings for quick testing when setting up the front end.

--- nm-eval-service/src/main/java/com/nm/controller/ThingsController.java
+++ nm-eval-service/src/main/java/com/nm/controller/ThingsController.java
@@ -1,13 +1,26 @@
 package com.nm.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.text.RandomStringGenerator;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
+@CrossOrigin
 public class ThingsController {
 
-	@RequestMapping("/getThings")
-	public String getThings() {
-		return "quick test endpoint";
+	@RequestMapping("/getStringThings")
+	public List<String> getStringThings() {
+		ArrayList<String> thingStringList= new ArrayList<>();
+		char [][] pairs = {{'a','z'},{'0','9'}};
+		RandomStringGenerator stringGenorator = new RandomStringGenerator.Builder().withinRange(pairs).build();
+		for(int i = 0; i < 50; i++) {
+			thingStringList.add(stringGenorator.generate(10));
+		}
+		return thingStringList;
 	}
 }
